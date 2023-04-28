8fa51afdcaa29c65add2e8305cce573a4fefe06a
manishReddevil <manishojhac3@gmail.com>
2023-04-28 10:35:17 +1000

    Added style and js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+function element_scroll() {
+    if (const element = document.getElementById("about");) {
+        element.scrollIntoView();
+    } else {
+        nav.classList.remove('active')
+    }
+  
+}
